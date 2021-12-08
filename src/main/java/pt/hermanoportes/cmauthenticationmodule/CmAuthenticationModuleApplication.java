e68fadb2cf2065f5b94f756940d16e70369381b4
Hermano Portes <29491729896@serpro.gov.br>
2021-12-08 11:35:28 -0300

    initial commit

---
+++ src/main/java/pt/hermanoportes/cmauthenticationmodule/CmAuthenticationModuleApplication.java
@@ -0,0 +1,13 @@
+package pt.hermanoportes.cmauthenticationmodule;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CmAuthenticationModuleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CmAuthenticationModuleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pt/hermanoportes/cmauthenticationmodule/CmAuthenticationModuleApplicationTests.java
@@ -0,0 +1,13 @@
+package pt.hermanoportes.cmauthenticationmodule;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CmAuthenticationModuleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
