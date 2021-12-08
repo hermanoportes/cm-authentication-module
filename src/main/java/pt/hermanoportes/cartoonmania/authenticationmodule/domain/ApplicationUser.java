ed37b2c7de3a238e98aab0cfa1a3374eb8ebf3ed
Hermano Portes <29491729896@serpro.gov.br>
2021-12-08 12:08:19 -0300

    Add ApplicationUser and Authority

---
+++ src/main/java/pt/hermanoportes/cartoonmania/authenticationmodule/domain/ApplicationUser.java
@@ -0,0 +1,60 @@
+package pt.hermanoportes.cartoonmania.authenticationmodule.domain;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
+@Getter @Setter
+@Entity
+public class ApplicationUser implements UserDetails {
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String username;
+    private String password;
+    private List<Authority> authorities = new ArrayList<>();
+
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return authorities;
+    }
+
+    @Override
+    public String getPassword() {
+        return this.password;
+    }
+
+    @Override
+    public String getUsername() {
+        return this.username;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+}
